7b72a2905caa885c8b969a985b81f354c65afd6d
Simen Haug <93390585+simhau19@users.noreply.github.com>
2025-11-07 15:47:40 +0100

    added keymap to search commands

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -75,6 +75,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
     vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
     vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
     vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
     vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
     -- Slightly advanced example of overriding default behavior and theme
@@ -100,7 +101,7 @@ return { -- Fuzzy Finder (files, lsp, etc)
       builtin.find_files { cwd = vim.fn.stdpath 'config' }
     end, { desc = '[S]earch [N]eovim files' })
 
-    -- Shortcut for searching through dotfiles files
+    -- Shortcut for searching through dotfiles
     vim.keymap.set('n', '<leader>s.', function()
       builtin.find_files { cwd = vim.fn.expand '$HOME/.dotfiles' }
     end, { desc = '[S]earch [.]dotfiles' })
