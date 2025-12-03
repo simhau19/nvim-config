9a00b0a66d2e94e0f987ea769608f9876c7d2fa9
Simen Haug <93390585+simhau19@users.noreply.github.com>
2025-12-03 16:57:15 +0100

    added shortcut to pin main in typst project

--- after/ftplugin/typst.lua
+++ after/ftplugin/typst.lua
@@ -1,5 +1,6 @@
 vim.keymap.set({ 'n', 'v' }, '<leader>tp', ':TypstPreviewToggle<CR>', { buffer = true, desc = '[T]oggle [P]review' })
 vim.keymap.set({ 'n', 'v' }, '<leader>pc', ':LspTinymistExportPdf<CR>', { buffer = true, desc = '[P]roject [C]ompile' })
+vim.keymap.set({ 'n', 'v' }, '<leader>pm', ':LspTinymistPinMain<CR>', { buffer = true, desc = '[P]in [M]ain' })
 
 vim.cmd [[
     setlocal linebreak
