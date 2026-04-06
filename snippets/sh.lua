3f217ee00788f566dcfe0e92fe9e812c5979cadf
Simen Haug <93390585+simhau19@users.noreply.github.com>
2026-04-06 15:17:51 +0200

    updated treesitter to work with nvim 0.12

---
+++ snippets/sh.lua
@@ -0,0 +1,7 @@
+-- cpp, hpp and h filetypes are all treated as cpp
+
+---@diagnostic disable: undefined-global
+
+return {
+  s({ trig = 'shebang', name = 'add shebang' }, t '#!/usr/bin/env sh'),
+}
