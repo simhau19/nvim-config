6f69ed98f6221eec7b22d9b67cdce55fdaf962f5
Simen Haug <93390585+simhau19@users.noreply.github.com>
2026-04-08 11:06:32 +0200

    added extra cmake keybindings

--- after/ftplugin/c.lua
+++ after/ftplugin/c.lua
@@ -10,9 +10,12 @@ end
 -- end
 
 vim.keymap.set({ 'n', 'v' }, '<leader>pg', ':CMakeGenerate<CR>', { buffer = true, desc = '[P]roject [G]enerate' })
-vim.keymap.set({ 'n', 'v' }, '<leader>pc', ':CMakeBuild<CR>', { buffer = true, desc = '[P]roject [C]ompile' })
+vim.keymap.set({ 'n', 'v' }, '<leader>pc', ':CMakeBuild<CR>', { buffer = true, desc = '[P]roject [c]ompile' })
+vim.keymap.set({ 'n', 'v' }, '<leader>pC', ':CMakeClean<CR>', { buffer = true, desc = '[P]roject [C]lean' })
 vim.keymap.set({ 'n', 'v' }, '<leader>pr', ':CMakeRun<CR>', { buffer = true, desc = '[P]roject [R]un' })
 vim.keymap.set({ 'n', 'v' }, '<leader>pd', ':CMakeDebug<CR>', { buffer = true, desc = '[P]roject [D]ebug' })
 vim.keymap.set({ 'n', 'v' }, '<leader>pp', ':CMakeSelectConfigurePreset<CR>', { buffer = true, desc = '[P]roject select [P]reset' })
 vim.keymap.set({ 'n', 'v' }, '<leader>psb', ':CMakeSelectBuildTarget<CR>', { buffer = true, desc = '[P]roject [S]elect [B]uild target' })
 vim.keymap.set({ 'n', 'v' }, '<leader>psl', ':CMakeSelectLaunchTarget<CR>', { buffer = true, desc = '[P]roject [S]elect [L]aunch target' })
+vim.keymap.set({ 'n', 'v' }, '<leader>poe', ':CMakeOpenExecutor<CR>', { buffer = true, desc = '[P]roject [O]pen [E]xecutor' })
+vim.keymap.set({ 'n', 'v' }, '<leader>por', ':CMakeOpenRunner<CR>', { buffer = true, desc = '[P]roject [O]pen [R]unner' })
